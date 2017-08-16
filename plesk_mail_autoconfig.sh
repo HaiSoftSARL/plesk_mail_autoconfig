75178175f27d84a397a330140235b6a8825cf2c1
UltimateByte <robin.labadie@gmail.com>
2017-08-16 16:28:33 +0200

    Autodiscover addition

--- plesk_mail_autoconfig.sh
+++ plesk_mail_autoconfig.sh
@@ -2,24 +2,63 @@
 # Deploy mail autoconfig for Plesk
 # Website: haisoft.fr
 
+# Autoconfig paths
 autoconfigpath="/var/www/vhosts/default/htdocs/mail"
 autoconfigfile="config-v1.1.xml"
 autoconfigpathfile="${autoconfigpath}/${autoconfigfile}"
 
-echo "Mail autoconfig generator for Plesk"
+# Autodiscover paths
+autodiscoverpath="/var/www/vhosts/default/htdocs/autodiscover"
+autodiscoverfile="autodiscover.xml"
+autodiscoverpathfile="${autodiscoverpath}/${autodiscoverfile}"
+autodiscoverhtaccess="${autodiscoverpath}/.htaccess"
+
+##############
+### Script ###
+##############
+
+# Misc Vars
+selfname="Mail Autoconfig"
+hostname="$(hostname)"
+
+# Download bash API
+if [ ! -f "ultimate-bash-api.sh" ]; then
+	wget https://raw.githubusercontent.com/UltimateByte/ultimate-bash-api/master/ultimate-bash-api.sh
+	chmod +x ultimate-bash-api.sh
+fi
+source ultimate-bash-api.sh
+
+
+echo "Mail autoconfig && autodiscover generator for Plesk"
 echo "###################################"
 
 if [ ! -d "${autoconfigpath}" ]; then
-	echo "[INFO] Creating autoconfig path"
+	fn_logecho "[INFO] Creating autoconfig cofngi path"
 	mkdir -pv "${autoconfigpath}"
 fi
 
 if [ ! -f "${autoconfigpathfile}" ]; then
-	echo "[INFO] Creating autoconfig file"
+	fn_logecho "[INFO] Creating autoconfig config file"
 	touch "${autoconfigpathfile}"
 fi
 
-echo "[INFO] Writing into autoconfig file"
+if [ ! -d "${autodiscoverpath}" ]; then
+	fn_logecho "[INFO] Creating autodiscover config path"
+	mkdir -pv "${autodiscoverpath}"
+fi
+
+if [ ! -f "${autodiscoverpathfile}" ]; then
+	fn_logecho "[INFO] Creating autodiscover config file"
+	touch "${autodiscoverpathfile}"
+fi
+
+if [ ! -f "${autodiscoverhtaccess}" ]; then
+	fn_logecho "[INFO] Creating autodiscover .htaccess file"
+	touch "${autodiscoverhtaccess}"
+fi
+
+
+fn_logecho "[INFO] Writing autoconfig config file"
 
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
 
@@ -31,7 +70,7 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
     <displayShortName>HaiSoft</displayShortName>
 
     <incomingServer type=\"imap\">
-      <hostname>$(hostname)</hostname>
+      <hostname>${hostname}</hostname>
       <port>993</port>
       <socketType>SSL</socketType>
       <authentication>password-encrypted</authentication>
@@ -39,7 +78,7 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
     </incomingServer>
 
     <incomingServer type=\"imap\">
-      <hostname>$(hostname)</hostname>
+      <hostname>${hostname}</hostname>
       <port>143</port>
       <socketType>STARTTLS</socketType>
       <authentication>password-encrypted</authentication>
@@ -47,7 +86,7 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
     </incomingServer>
 
     <incomingServer type=\"pop3\">
-      <hostname>$(hostname)</hostname>
+      <hostname>${hostname}</hostname>
       <port>995</port>
       <socketType>SSL</socketType>
       <authentication>password-encrypted</authentication>
@@ -55,7 +94,7 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
     </incomingServer>
 
     <incomingServer type=\"pop3\">
-      <hostname>$(hostname)</hostname>
+      <hostname>${hostname}</hostname>
       <port>110</port>
       <socketType>STARTTLS</socketType>
       <authentication>password-encrypted</authentication>
@@ -63,7 +102,7 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
     </incomingServer>
 
     <outgoingServer type=\"smtp\">
-      <hostname>$(hostname)</hostname>
+      <hostname>${hostname}</hostname>
       <port>465</port>
       <socketType>SSL</socketType>
       <authentication>password-encrypted</authentication>
@@ -71,7 +110,7 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
     </outgoingServer>
 
     <outgoingServer type=\"smtp\">
-      <hostname>$(hostname)</hostname>
+      <hostname>${hostname}</hostname>
       <port>587</port>
       <socketType>STARTTLS</socketType>
       <authentication>password-encrypted</authentication>
@@ -79,7 +118,7 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
     </outgoingServer>
 
     <outgoingServer type=\"smtp\">
-      <hostname>$(hostname)</hostname>
+      <hostname>${hostname}</hostname>
       <port>25</port>
       <socketType>STARTTLS</socketType>
       <authentication>password-encrypted</authentication>
@@ -87,7 +126,7 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
     </outgoingServer>
 
     <outgoingServer type=\"smtp\">
-      <hostname>$(hostname)</hostname>
+      <hostname>${hostname}</hostname>
       <port>2525</port>
       <socketType>STARTTLS</socketType>
       <authentication>password-encrypted</authentication>
@@ -102,19 +141,61 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
   </emailProvider>
 </clientConfig>" > "${autoconfigpathfile}"
 
-echo "[INFO] Correcting default DNS zone"
-/usr/local/psa/bin/server_dns --add -cname autoconfig -canonical "$(hostname)"
+fn_logecho "[INFO] Correcting default DNS zone for autoconfig"
+/usr/local/psa/bin/server_dns --add -cname autoconfig -canonical "${hostname}"
 
-echo "[INFO] Adding DNS entry for every website"
+fn_logecho "[INFO] Adding DNS entry for every website for autoconfig"
 
 for i in `mysql -uadmin -p\`cat /etc/psa/.psa.shadow\` psa -Ns -e "select name from domains"`; do 
-	/usr/local/psa/bin/dns --add "$i" -cname autoconfig -canonical "$(hostname)"
+	/usr/local/psa/bin/dns --add "$i" -cname autoconfig -canonical "${hostname}"
 done
 
-if [ -n "$(curl "http://autoconfig.$(hostname)/mail/config-v1.1.xml" | grep "<socketType>SSL</socketType>")" ]; then
-	echo "[OK] http://autoconfig.$(hostname)/mail/config-v1.1.xml is accessible"
+fn_logecho "[INFO] Writing autodiscover config file"
+echo "<?php
+$raw = file_get_contents('php://input');
+$matches = array();
+preg_match('/<EMailAddress>(.*)<\/EMailAddress>/', $raw, $matches);
+header('Content-Type: application/xml');
+?>
+<Autodiscover xmlns="http://schemas.microsoft.com/exchange/autodiscover/responseschema/2006">
+  <Response xmlns="http://schemas.microsoft.com/exchange/autodiscover/outlook/responseschema/2006a">
+    <User>
+      <DisplayName>HaiSoft</DisplayName>
+    </User>
+    <Account>
+      <AccountType>email</AccountType>
+      <Action>settings</Action>
+      <Protocol>
+        <Type>IMAP</Type>
+        <Server>${hostname}</Server>
+        <Port>993</Port>
+        <DomainRequired>off</DomainRequired>
+        <SPA>off</SPA>
+        <SSL>on</SSL>
+        <AuthRequired>on</AuthRequired>
+        <LoginName><?php echo $matches[1]; ?></LoginName>
+      </Protocol>
+      <Protocol>
+        <Type>SMTP</Type>
+        <Server>${hostname}</Server>
+        <Port>465</Port>
+        <DomainRequired>off</DomainRequired>
+        <SPA>off</SPA>
+        <SSL>on</SSL>
+        <AuthRequired>on</AuthRequired>
+        <LoginName><?php echo $matches[1]; ?></LoginName>
+      </Protocol>
+    </Account>
+  </Response>
+</Autodiscover>" > "${autodiscoverpathfile}"
+
+fn_logecho "[INFO] Writing autodiscover htaccess"
+echo "AddHandler php-script .php .xml" > "${autodiscoverhtaccess}"
+
+if [ -n "$(curl "http://autoconfig.${hostname}/mail/config-v1.1.xml" | grep "<socketType>SSL</socketType>")" ]; then
+	fn_logecho "[OK] http://autoconfig.${hostname}/mail/config-v1.1.xml is accessible"
 else
-	echo "[ERROR!] http://autoconfig.$(hostname)/mail/config-v1.1.xml does not seem to be accessible"
+	fn_logecho "[ERROR!] http://autoconfig.${hostname}/mail/config-v1.1.xml does not seem to be accessible"
 fi
 
-echo "[INFO] Done"
+fn_logecho "[INFO] Done"
